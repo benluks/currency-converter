1c677ecd9ddaaaa5deac25dfe20ae34a08d7b601
Ben Luks <ben@Bens-MacBook-Air.local>
2020-11-03 09:44:49 -0500

    Updates

---
+++ precache-manifest.fd9c6341a5fb6b828ca5becd92568f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd06f3955bfad79ec3486d8c0d170ae1",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "9b14f888de7805f79155",
+    "url": "/currency-converter/static/css/main.6f11d590.chunk.css"
+  },
+  {
+    "revision": "48211dc59cca9e7644af",
+    "url": "/currency-converter/static/js/2.f86cfd3f.chunk.js"
+  },
+  {
+    "revision": "836e51deb380785bea2031ce09dc567c",
+    "url": "/currency-converter/static/js/2.f86cfd3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b14f888de7805f79155",
+    "url": "/currency-converter/static/js/main.3de3c1b8.chunk.js"
+  },
+  {
+    "revision": "7dc36315c1c9557d24ce",
+    "url": "/currency-converter/static/js/runtime-main.a820bd86.js"
+  }
+]);
